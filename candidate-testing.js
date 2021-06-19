2cd7cc964c111586469fe5941d64d7357780878a
Kenneth Jackson <kcjackson1985@gmail.com>
2021-06-19 21:47:15 +0000

    Defined questions and correctAnswers variables as arrays.

--- candidate-testing.js
+++ candidate-testing.js
@@ -5,8 +5,10 @@ const input = require('readline-sync');
 // TODO 1.1a: Define candidateName // 
 let candidateName = "";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question = "Who was the first American woman in space? ";
-let correctAnswer = "Sally Ride";
+let question = ["Who was the first American woman in space? ","True or false: 5 kilometer == 5000 meters ",
+                "(5 + 3)/2 * 10 = ? ","Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
+                "What is the minimum crew size for the ISS? "];
+let correctAnswer = ["Sally Ride","true","40","Trajectory","3"];
 let candidateAnswer = "";
 let questions;
 let correctAnswers;
